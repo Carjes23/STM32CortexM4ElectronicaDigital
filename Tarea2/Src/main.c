483d1ea389412d23edef762ff59b50cb11b075f3
dacardenasj <dacardenasj@unal.edu.co>
2023-03-11 22:55:51 -0500

    Modificacion en el nombre de los Handlers

--- Tarea2/Src/main.c
+++ Tarea2/Src/main.c
@@ -71,87 +71,95 @@ int main(void){
 
 	//DEFINICION PINES PUNTO 3.
 	GPIO_Handler_t handlerPC9= {0};
-	handlerUserLedPin.pGPIOx = GPIOC;
-	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinNumber		= PIN_9;
-	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinMode			= GPIO_MODE_OUT;
-	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinOPType		= GPIO_OTYPE_PUSHPULL;
-	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinPuPdControl	= GPIO_PUPDR_NOTHING;
-	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinSpeed		= GPIO_OSPEED_MEDIUM;
-	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinAltFunMode	= AF0; //Ninuna funcion.
+	handlerPC9.pGPIOx = GPIOC;
+	handlerPC9.GPIO_PinConfig_t.GPIO_PinNumber		= PIN_9;
+	handlerPC9.GPIO_PinConfig_t.GPIO_PinMode			= GPIO_MODE_OUT;
+	handlerPC9.GPIO_PinConfig_t.GPIO_PinOPType		= GPIO_OTYPE_PUSHPULL;
+	handlerPC9.GPIO_PinConfig_t.GPIO_PinPuPdControl	= GPIO_PUPDR_NOTHING;
+	handlerPC9.GPIO_PinConfig_t.GPIO_PinSpeed		= GPIO_OSPEED_MEDIUM;
+	handlerPC9.GPIO_PinConfig_t.GPIO_PinAltFunMode	= AF0; //Ninuna funcion.
+	GPIO_Config(&handlerPC9);
 
 	GPIO_Handler_t handlerPC6= {0};
-	handlerUserLedPin.pGPIOx = GPIOC;
-	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinNumber		= PIN_6;
-	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinMode			= GPIO_MODE_OUT;
-	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinOPType		= GPIO_OTYPE_PUSHPULL;
-	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinPuPdControl	= GPIO_PUPDR_NOTHING;
-	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinSpeed		= GPIO_OSPEED_MEDIUM;
-	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinAltFunMode	= AF0; //Ninuna funcion.
+	handlerPC6.pGPIOx = GPIOC;
+	handlerPC6.GPIO_PinConfig_t.GPIO_PinNumber		= PIN_6;
+	handlerPC6.GPIO_PinConfig_t.GPIO_PinMode			= GPIO_MODE_OUT;
+	handlerPC6.GPIO_PinConfig_t.GPIO_PinOPType		= GPIO_OTYPE_PUSHPULL;
+	handlerPC6.GPIO_PinConfig_t.GPIO_PinPuPdControl	= GPIO_PUPDR_NOTHING;
+	handlerPC6.GPIO_PinConfig_t.GPIO_PinSpeed		= GPIO_OSPEED_MEDIUM;
+	handlerPC6.GPIO_PinConfig_t.GPIO_PinAltFunMode	= AF0; //Ninuna funcion.
+	GPIO_Config(&handlerPC6);
 
 	GPIO_Handler_t handlerPB8= {0};
-	handlerUserLedPin.pGPIOx = GPIOB;
-	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinNumber		= PIN_8;
-	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinMode			= GPIO_MODE_OUT;
-	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinOPType		= GPIO_OTYPE_PUSHPULL;
-	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinPuPdControl	= GPIO_PUPDR_NOTHING;
-	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinSpeed		= GPIO_OSPEED_MEDIUM;
-	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinAltFunMode	= AF0; //Ninuna funcion.
+	handlerPB8.pGPIOx = GPIOB;
+	handlerPB8.GPIO_PinConfig_t.GPIO_PinNumber		= PIN_8;
+	handlerPB8.GPIO_PinConfig_t.GPIO_PinMode			= GPIO_MODE_OUT;
+	handlerPB8.GPIO_PinConfig_t.GPIO_PinOPType		= GPIO_OTYPE_PUSHPULL;
+	handlerPB8.GPIO_PinConfig_t.GPIO_PinPuPdControl	= GPIO_PUPDR_NOTHING;
+	handlerPB8.GPIO_PinConfig_t.GPIO_PinSpeed		= GPIO_OSPEED_MEDIUM;
+	handlerPB8.GPIO_PinConfig_t.GPIO_PinAltFunMode	= AF0; //Ninuna funcion.
+	GPIO_Config(&handlerPB8);
 
 	GPIO_Handler_t handlerPA6= {0};
-	handlerUserLedPin.pGPIOx = GPIOA;
-	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinNumber		= PIN_6;
-	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinMode			= GPIO_MODE_OUT;
-	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinOPType		= GPIO_OTYPE_PUSHPULL;
-	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinPuPdControl	= GPIO_PUPDR_NOTHING;
-	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinSpeed		= GPIO_OSPEED_MEDIUM;
-	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinAltFunMode	= AF0; //Ninuna funcion.
+	handlerPA6.pGPIOx = GPIOA;
+	handlerPA6.GPIO_PinConfig_t.GPIO_PinNumber		= PIN_6;
+	handlerPA6.GPIO_PinConfig_t.GPIO_PinMode			= GPIO_MODE_OUT;
+	handlerPA6.GPIO_PinConfig_t.GPIO_PinOPType		= GPIO_OTYPE_PUSHPULL;
+	handlerPA6.GPIO_PinConfig_t.GPIO_PinPuPdControl	= GPIO_PUPDR_NOTHING;
+	handlerPA6.GPIO_PinConfig_t.GPIO_PinSpeed		= GPIO_OSPEED_MEDIUM;
+	handlerPA6.GPIO_PinConfig_t.GPIO_PinAltFunMode	= AF0; //Ninuna funcion.
+	GPIO_Config(&handlerPA6);
 
 	GPIO_Handler_t handlerPC7= {0};
-	handlerUserLedPin.pGPIOx = GPIOC;
-	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinNumber		= PIN_7;
-	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinMode			= GPIO_MODE_OUT;
-	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinOPType		= GPIO_OTYPE_PUSHPULL;
-	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinPuPdControl	= GPIO_PUPDR_NOTHING;
-	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinSpeed		= GPIO_OSPEED_MEDIUM;
-	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinAltFunMode	= AF0; //Ninuna funcion.
+	handlerPC7.pGPIOx = GPIOC;
+	handlerPC7.GPIO_PinConfig_t.GPIO_PinNumber		= PIN_7;
+	handlerPC7.GPIO_PinConfig_t.GPIO_PinMode			= GPIO_MODE_OUT;
+	handlerPC7.GPIO_PinConfig_t.GPIO_PinOPType		= GPIO_OTYPE_PUSHPULL;
+	handlerPC7.GPIO_PinConfig_t.GPIO_PinPuPdControl	= GPIO_PUPDR_NOTHING;
+	handlerPC7.GPIO_PinConfig_t.GPIO_PinSpeed		= GPIO_OSPEED_MEDIUM;
+	handlerPC7.GPIO_PinConfig_t.GPIO_PinAltFunMode	= AF0; //Ninuna funcion.
+	GPIO_Config(&handlerPC7);
 
 	GPIO_Handler_t handlerPC8= {0};
-	handlerUserLedPin.pGPIOx = GPIOC;
-	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinNumber		= PIN_8;
-	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinMode			= GPIO_MODE_OUT;
-	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinOPType		= GPIO_OTYPE_PUSHPULL;
-	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinPuPdControl	= GPIO_PUPDR_NOTHING;
-	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinSpeed		= GPIO_OSPEED_MEDIUM;
-	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinAltFunMode	= AF0; //Ninuna funcion.
+	handlerPC8.pGPIOx = GPIOC;
+	handlerPC8.GPIO_PinConfig_t.GPIO_PinNumber		= PIN_8;
+	handlerPC8.GPIO_PinConfig_t.GPIO_PinMode			= GPIO_MODE_OUT;
+	handlerPC8.GPIO_PinConfig_t.GPIO_PinOPType		= GPIO_OTYPE_PUSHPULL;
+	handlerPC8.GPIO_PinConfig_t.GPIO_PinPuPdControl	= GPIO_PUPDR_NOTHING;
+	handlerPC8.GPIO_PinConfig_t.GPIO_PinSpeed		= GPIO_OSPEED_MEDIUM;
+	handlerPC8.GPIO_PinConfig_t.GPIO_PinAltFunMode	= AF0; //Ninuna funcion.
+	GPIO_Config(&handlerPC8);
 
 	GPIO_Handler_t handlerPA7= {0};
-	handlerUserLedPin.pGPIOx = GPIOA;
-	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinNumber		= PIN_7;
-	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinMode			= GPIO_MODE_OUT;
-	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinOPType		= GPIO_OTYPE_PUSHPULL;
-	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinPuPdControl	= GPIO_PUPDR_NOTHING;
-	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinSpeed		= GPIO_OSPEED_MEDIUM;
-	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinAltFunMode	= AF0; //Ninuna funcion.
+	handlerPA7.pGPIOx = GPIOA;
+	handlerPA7.GPIO_PinConfig_t.GPIO_PinNumber		= PIN_7;
+	handlerPA7.GPIO_PinConfig_t.GPIO_PinMode			= GPIO_MODE_OUT;
+	handlerPA7.GPIO_PinConfig_t.GPIO_PinOPType		= GPIO_OTYPE_PUSHPULL;
+	handlerPA7.GPIO_PinConfig_t.GPIO_PinPuPdControl	= GPIO_PUPDR_NOTHING;
+	handlerPA7.GPIO_PinConfig_t.GPIO_PinSpeed		= GPIO_OSPEED_MEDIUM;
+	handlerPA7.GPIO_PinConfig_t.GPIO_PinAltFunMode	= AF0; //Ninuna funcion.
+	GPIO_Config(&handlerPA7);
 
 	/*
 	 * 1)
 	 * El error en GPIO_ReadPin venia dado porque no se tenian en cuenta los valores
-	 * que estaban por delante del pin, por lo que si se tenian valores diferentes
-	 * a 0, delante del pin objetivo (el que queremos leer) se entragaria un valor
+	 * que estaban por delante del numero de pin, por lo que si se tenian valores diferentes
+	 * a 0 delante del pin objetivo (el que queremos leer) se entregaria un valor
 	 * erroneo, por esto se tiene que realizar un limpiado previo, este se realiza con
 	 * un and sobre el registro junto con una mascara con 0 en todas las posiciones
 	 * excepto en la poisicion objetivo, en el GPIOxDrive.c se pueden ver los cambios que
 	 * se realizaron(luego mientras se hacia la tarea se descubrio que bastaba solo con un
-	 * agragarle un & con el SET para  consevar solo el primer valor, de todos modos se deja
-	 * con la implementacion anterior en el GPIOxDriver.c)
-	 *  y el siguiente codigo demuestra su funcionamiento al programar
+	 * agragarle un and con SET para  consevar solo el primer valor, de todos modos se deja
+	 * con la implementacion anterior en el GPIOxDriver.c) y el siguiente codigo demuestra su funcionamiento al programar
 	 * el boton de usuario (azul) para que prenda y apague el LED LD2, leyendo el valor
 	 * tanto del boton azul, como del led, se crea una funcion sleep() que me permita
-	 * "dormir" el codigo por unos segundos para que halla como un tiempo donde no se cambie
-	 * el valor, para poder ver los cambios (los pasos por segundos definidos son completamente
+	 * "dormir" el codigo por un segundo para que halla como un tiempo donde no se cambie
+	 * el valor, para poder ver los cambios. (Los pasos por segundos definidos son completamente
 	 * empiricos a ensayo y error).
 	 * 2) En el mismo codigo que prende y apaga el led con el boton se utiliza la funcion
 	 * GGPIOxTooglePin
+	 * 3) El punto 3 se desarrollara en el mismo while pero teniendo en cuenta
+	 * que se tiene que comentar la parte del codigo de apagar y prender el LED.
 	 */
 	uint8_t contador = 1;
 	bool PC9 = 0, PC6 = 0, PB8 = 0, PA6 = 0, PC7 = 0, PC8 = 0, PA7 = 0;
