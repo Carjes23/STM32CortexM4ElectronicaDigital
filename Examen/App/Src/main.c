9e4e4d3aadd0ce09ba89f6088ccdc3edfd109a5c
Carjes23 <dacardenasj@unal.edu.co>
2023-06-10 15:47:36 -0500

    Se agrega el CMSIS FUll y se adelanta la tarea dejandola casi terminada, falta comentarios y detalles.

--- Examen/App/Src/main.c
+++ Examen/App/Src/main.c
@@ -27,6 +27,7 @@
  ******************************************************************************
  */
 
+//Drivers usados para el examen
 #include "GPIOxDriver.h"
 #include "USARTxDriver.h"
 #include "BasicTimer.h"
@@ -38,6 +39,8 @@
 #include "RTCDriver.h"
 #include "I2CxDriver.h"
 #include "SysTick.h"
+
+#include "arm_math.h"
 
 //Se definen algunos registros importantes del acelerrometro
 #define ACCEL_ADDRESS 0x1D		//0xD -> Rireccion del Accel con Logic_1
@@ -65,7 +68,8 @@
 #define ADXL345_RANGE_4_G 0b01  ///< +/- 4g
 #define ADXL345_RANGE_2_G 0b00   ///< +/- 2g (default value)
 
-void getFecha(void);
+//Vector que apunta a los datos input de la FFT.
+float *datos;
 
 /*
  * Variables definidas para la tarea.
@@ -79,24 +83,34 @@ uint8_t AccelX_low = 0;
 uint8_t AccelX_high = 0;
 int16_t AccelX = 0;
 float AccelXEsc = 0.0f;
-float xmean = 0.f;
-uint8_t ofsX = 0;
 
 //Variables para el tratamento de la aceleración en y
 uint8_t AccelY_low = 0;
 uint8_t AccelY_high = 0;
 int16_t AccelY = 0;
 float AccelYEsc = 0.0f;
-float ymean = 0.f;
-uint8_t ofsY = 0;
 
 //Variables para el tratamento de la aceleración en z
 uint8_t AccelZ_low = 0;
 uint8_t AccelZ_high = 0;
 int16_t AccelZ = 0;
 float AccelZEsc = 0.0f;
-float zmean = 0.f;
-uint8_t ofsZ = 0;
+
+//VAriables relacionadas al acelerometro.
+
+uint16_t freqAdc = 20;
+
+float transformedSignal[256];
+float transformedAbs[256];
+
+float32_t stopTime = 1.0;
+uint32_t ifftFlag = 0;
+uint32_t doBitReverse = 1;
+arm_rfft_fast_instance_f32 config_Rfft_fast_f32;
+arm_cfft_radix4_instance_f32 configRadix4_f32;
+arm_status status = ARM_MATH_ARGUMENT_ERROR;
+arm_status statusInitFFT = ARM_MATH_ARGUMENT_ERROR;
+uint16_t fttSize = 256;
 
 //Handler I2Cs
 I2C_Handler_t i2cAcelerometro = { 0 }; //I2C encargado de comunicarse con el acelerometro
@@ -109,83 +123,109 @@ GPIO_Handler_t I2cSCL = { 0 }; // Handler que manipula la frecuencia de comunica
 
 GPIO_Handler_t handlerUserLedPin = { 0 };
 GPIO_Handler_t handlerUserLedPin2 = { 0 };
-GPIO_Handler_t rx2pin = { 0 };
-GPIO_Handler_t tx2pin = { 0 };
+GPIO_Handler_t tx6pin = { 0 };
+GPIO_Handler_t rx6pin = { 0 };
 GPIO_Handler_t pwprueba = { 0 };
 GPIO_Handler_t handlerPinMCO = { 0 };
 
-//Timers
+//Diversos buffer para la recepcion y transmision de datos
 
 char bufferReception[64] = { 0 };
 char cmd[64] = { 0 };
-unsigned int firstParameter = 0;
-unsigned int secondParameter = 0;
+unsigned int firstParameter = 256;
+unsigned int secondParameter = 256;
 char userMsg[64] = { 0 };
 
-BasicTimer_Handler_t handlerTimer2 = { 0 };
-BasicTimer_Handler_t handlerTimer5 = { 0 };
+//Timers
 
+BasicTimer_Handler_t handlerTimer2 = { 0 }; // Timer para el blinking
+BasicTimer_Handler_t handlerTimer5 = { 0 }; //Timer para la toma de datos
+
+//Handler para el control de la terminal
 USART_Handler_t handlerTerminal = { 0 };
-
+//Para la recepcion de datos por la terminal
 uint8_t rxData = 0;
 
+//Funciones para inciar el sistema
 void initSystem(void);
+//Funcion para tratar los comandos
 void parseCommands(char *ptrBufferReception);
+//Funcion que actuailiza la fecha
+void getFecha(void);
 
+//Funcion para cuadrar el ADC
 ADC_Config_t channnel_0 = { 0 };
-
+//Variables relacionadas al adc
 uint8_t ADCISCOMPLETE = 0;
 uint16_t adcData[2][256] = { 0 };
+float adcData2[2][256] = { 0 };
+//Variable para cambiar la sintonizacion del HSI
 uint8_t trimValue = 13;
+//Ultimos datos de cada ADC
 uint16_t adcLastData1 = 0;
 uint16_t adcLastData2 = 0;
 
+//Vanderas para manejar estados.
 bool banderaADC = 0;
 bool adcIsComplete = 0;
 bool banderaImprimir = 0;
 bool banderaImprimirAct = 0;
 bool stringComplete = 0;
 bool flagDatos = 0;
-bool flag200Listos = 0;
+bool flag256Listos = 0;
+bool pllTrue = 0;
+bool flagDatosTer = 0;
+//Registros para la lecutra del acelerometro
 uint8_t regDatos[6] = { ACCEL_XOUT_L, ACCEL_XOUT_H, ACCEL_YOUT_L, ACCEL_YOUT_H,
 ACCEL_ZOUT_L, ACCEL_ZOUT_H };
+//Bandera que cuenta el adc
 uint8_t adcIsCompleteCount = 0;
 uint16_t counterReception = 0;
-bool pllTrue = 0;
-uint16_t contador200 = 0;
-bool flagDatosTer = 0;
-float DatoX200[200] = { 0 };
-float DatoY200[200] = { 0 };
-float DatoZ200[200] = { 0 };
-uint16_t contadorImprimir200 = 0;
+uint16_t contador256 = 0;
+//Arreglos para guardar los datos
+float DatoX256[256] = { 0 };
+float DatoY256[256] = { 0 };
+float DatoZ256[256] = { 0 };
+
+uint16_t contadorImprimir256 = 0; //Para imprimir los 256 datos
 
 uint16_t i2cBuffer = 0;		 //Buffer para la recepción de datos en el I2C
 
-uint32_t count256 = 0;
+uint32_t count256 = 0;		//Para guardar los d¿256 datos
 
 /*
  * Respuesta del acelerometro al solicitarle datos.
  */
-uint8_t resDatos[6] = { 0 };
+uint8_t resDatos[6] = { 0 };	//Para recibir los resultados del acelerometro
 
 PWM_Handler_t pwmadc = { 0 }; //Para configurar el PWM en el timer 3 para X
 
-char bufferData[128] = { 0 };
+char bufferData[128] = { 0 };	//para crear mensajes
 
-uint8_t segundero = 0;
+uint8_t segundero = 0;		//"Bandera" que nos permite actulalizar el reloj
 
-char dia[10] = { 0 };
+char dia[10] = { 0 };		//Nimbre del día actual
 
 int main(void) {
+	//Se inicia el sistema
 	initSystem();
+	//Los delays es para que no se solapen en el Acelerometro
 	delay_ms(15);
 	writeString(&handlerTerminal, "Solución Examen \n");
 	i2c_writeSingleRegister(&i2cAcelerometro, POWER_CTL, 0x08);
-	writeString(&handlerTerminal, "El acelerometro se encuentra listo para su funcionamiento. \n");
-	writeString(&handlerTerminal, "Para conocer todos los comandos disponibles usar help @ \n");
+	delay_ms(1);
+	//Colocamos la frecuencia de muestreo del accel a 3600 hz
+	i2c_writeSingleRegister(&i2cAcelerometro, ACCEL_BW_RATE, 0x0F);
+	/*
+	 * Se envian mensajes de inicio
+	 */
+	writeString(&handlerTerminal,
+			"El acelerometro se encuentra listo para su funcionamiento. \n");
+	writeString(&handlerTerminal,
+			"Para conocer todos los comandos disponibles usar help @ \n");
 	while (1) {
-
-		if (segundero > 0) {
+		// Cada 500 ms se actualza
+		if (segundero > 2) {
 			getFecha();
 			segundero = 0;
 		}
@@ -197,13 +237,17 @@ int main(void) {
 
 			// If the incoming character is a newline, set a flag
 			// so the main loop can do something about it
-			if (rxData == '@') {
+			if (rxData == '\r') {
 				stringComplete = 1;
 				sprintf(bufferData, "\n");
 				writeString(&handlerTerminal, bufferData);
 				//Agrego esta linea para crear el string con el null al final
 				bufferReception[counterReception] = '\0';
 				counterReception = 0;
+			}
+			if(rxData == '\b'){
+				counterReception--;
+				counterReception--;
 			}
 			//Para que no vuelva entrar. Solo cambia debido a la interrupcion
 			rxData = '\0';
@@ -214,17 +258,26 @@ int main(void) {
 			parseCommands(bufferReception);
 			stringComplete = 0;
 		}
-
+		//Se imrpimen los 256 datos del dc
 		if (banderaImprimir) {
 			for (int i = 0; i < 256; i++) {
 				sprintf(bufferData, "%u\t%u \n", adcData[0][i], adcData[1][i]);
+				writeString(&handlerTerminal, bufferData);
+			}
+			//Inicialización FFT
+			statusInitFFT = arm_rfft_fast_init_f32(&config_Rfft_fast_f32,
+					fttSize);
+
+			if (statusInitFFT == ARM_MATH_SUCCESS) {
+				//Imprimir mensaje de exito
+				sprintf(bufferData, "Datos adquiridos \n");
 				writeString(&handlerTerminal, bufferData);
 			}
 			banderaImprimir = 0;
 		}
 
 		/*
-		 * Este if se activa siempre y cuando ya tengamos los 200 datos para imprimir por
+		 * Este if se activa siempre y cuando ya tengamos los 256 datos para imprimir por
 		 * Usart
 		 */
 		if (flagDatosTer == 1) {
@@ -232,7 +285,7 @@ int main(void) {
 			 * Para el primer paso mandamos el formato como se entregando los datos, luego se empiezan
 			 * a recorrer los arreglos para que impriman todos los datos
 			 */
-			writeStringInt(&handlerTerminal, "Datos listos");
+			writeStringInt(&handlerTerminal, "Datos obtenidos\n");
 			flagDatosTer = 0;
 		}
 
@@ -269,17 +322,33 @@ int main(void) {
 			 * con la funcioón vista antes
 			 */
 
-			if (flag200Listos == 1) {
-				if (contador200 < 200) {
-					DatoX200[contador200] = AccelXEsc;
-					DatoY200[contador200] = AccelYEsc;
-					DatoZ200[contador200] = AccelZEsc;
+			if (flag256Listos == 1) {
+				if (contador256 < 256) {
+					DatoX256[contador256] = AccelXEsc;
+					DatoY256[contador256] = AccelYEsc;
+					DatoZ256[contador256] = AccelZEsc;
 
-					contador200++;
+					contador256++;
 				} else {
-					contador200 = 0;
-					flag200Listos = 0;
+					contador256 = 0;
+					flag256Listos = 0;
 					flagDatosTer = 1;
+					//Inicialización FFT
+					statusInitFFT = arm_rfft_fast_init_f32(
+							&config_Rfft_fast_f32, fttSize);
+
+					if (statusInitFFT == ARM_MATH_SUCCESS) {
+						//Imprimir mensaje de exito
+						sprintf(bufferData, "Datos adquiridos \n");
+						writeString(&handlerTerminal, bufferData);
+					}
+
+					// Imprimir el areglo de 256 datos
+					for (int i = 0; i < 256; i++) {
+						sprintf(bufferData, "n=%d x=%.2f y=%.2f z=%.2f\n", i,
+								DatoX256[i], DatoY256[i], DatoZ256[i]);
+						writeString(&handlerTerminal, bufferData);
+					}
 				}
 			}
 			//Se cierra la bandera de toma de datos.
@@ -334,7 +403,9 @@ void initSystem(void) {
 	i2cAcelerometro.slaveAddress = ACCEL_ADDRESS;
 	i2cAcelerometro.ptrI2Cx = I2C1;
 	i2c_config(&i2cAcelerometro);
-
+	/*
+	 * Leds de estado
+	 */
 	handlerUserLedPin.pGPIOx = GPIOH; //Se encuentra en el el GPIOH
 	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinNumber = PIN_1; // Es el pin 1
 	handlerUserLedPin.GPIO_PinConfig_t.GPIO_PinMode = GPIO_MODE_OUT; //Se utiliza como salida
@@ -355,23 +426,24 @@ void initSystem(void) {
 	GPIO_Config(&handlerUserLedPin2); //Se carga la configuración para el Led.
 
 //PA2
-	tx2pin.pGPIOx = GPIOA;
-	tx2pin.GPIO_PinConfig_t.GPIO_PinNumber = PIN_2;
-	tx2pin.GPIO_PinConfig_t.GPIO_PinMode = GPIO_MODE_ALTFN;
-	tx2pin.GPIO_PinConfig_t.GPIO_PinPuPdControl = GPIO_PUPDR_PULLUP;
-	tx2pin.GPIO_PinConfig_t.GPIO_PinSpeed = GPIO_OSPEED_FAST; //Se usa en velocidad rapida
-	tx2pin.GPIO_PinConfig_t.GPIO_PinAltFunMode = AF7;
+	//PA 11 -> 7 bajando -> Tx amarillo
+	tx6pin.pGPIOx = GPIOA;
+	tx6pin.GPIO_PinConfig_t.GPIO_PinNumber = PIN_11;
+	tx6pin.GPIO_PinConfig_t.GPIO_PinMode = GPIO_MODE_ALTFN;
+	tx6pin.GPIO_PinConfig_t.GPIO_PinPuPdControl = GPIO_PUPDR_PULLUP;
+	tx6pin.GPIO_PinConfig_t.GPIO_PinSpeed = GPIO_OSPEED_FAST; //Se usa en velocidad rapida
+	tx6pin.GPIO_PinConfig_t.GPIO_PinAltFunMode = AF8;
 
-	GPIO_Config(&tx2pin);
+	GPIO_Config(&tx6pin);
+	//PA 12 -> 6 bajando - Rx marron
+	rx6pin.pGPIOx = GPIOA;
+	rx6pin.GPIO_PinConfig_t.GPIO_PinNumber = PIN_12;
+	rx6pin.GPIO_PinConfig_t.GPIO_PinMode = GPIO_MODE_ALTFN;
+	rx6pin.GPIO_PinConfig_t.GPIO_PinPuPdControl = GPIO_PUPDR_PULLUP;
+	rx6pin.GPIO_PinConfig_t.GPIO_PinSpeed = GPIO_OSPEED_FAST;
+	rx6pin.GPIO_PinConfig_t.GPIO_PinAltFunMode = AF8;
 
-	rx2pin.pGPIOx = GPIOA;
-	rx2pin.GPIO_PinConfig_t.GPIO_PinNumber = PIN_3;
-	rx2pin.GPIO_PinConfig_t.GPIO_PinMode = GPIO_MODE_ALTFN;
-	rx2pin.GPIO_PinConfig_t.GPIO_PinPuPdControl = GPIO_PUPDR_PULLUP;
-	rx2pin.GPIO_PinConfig_t.GPIO_PinSpeed = GPIO_OSPEED_FAST;
-	rx2pin.GPIO_PinConfig_t.GPIO_PinAltFunMode = AF7;
-
-	GPIO_Config(&rx2pin);
+	GPIO_Config(&rx6pin);
 
 	handlerTimer2.ptrTIMx = TIM2; //El timer que se va a usar
 	handlerTimer2.TIMx_Config.TIMx_interruptEnable = 1; //Se habilitan las interrupciones
@@ -391,7 +463,7 @@ void initSystem(void) {
 
 	BasicTimer_Config(&handlerTimer2); //Se carga la configuración.
 
-	handlerTerminal.ptrUSARTx = USART2;
+	handlerTerminal.ptrUSARTx = USART6;
 	handlerTerminal.USART_Config.USART_baudrate = USART_BAUDRATE_115200;
 	handlerTerminal.USART_Config.USART_datasize = USART_DATASIZE_8BIT;
 	handlerTerminal.USART_Config.USART_mode = USART_MODE_RXTX;
@@ -400,7 +472,7 @@ void initSystem(void) {
 	handlerTerminal.USART_Config.USART_RX_Int_Ena = ENABLE;
 
 	USART_Config(&handlerTerminal);
-//Configuración Timer 2 que controlara el blinking
+//Configuración del pwm del timer 5 que controla el ADC
 
 	pwmadc.ptrTIMx = TIM5;
 	pwmadc.config.channel = PWM_CHANNEL_1;
@@ -412,6 +484,7 @@ void initSystem(void) {
 	enableOutput(&pwmadc);
 	startPwmSignal(&pwmadc);
 
+	//Salida para ver la velocidad de muestreo del ADC
 	pwprueba.pGPIOx = GPIOA;
 	pwprueba.GPIO_PinConfig_t.GPIO_PinNumber = PIN_0;
 	pwprueba.GPIO_PinConfig_t.GPIO_PinMode = GPIO_MODE_ALTFN;
@@ -422,14 +495,15 @@ void initSystem(void) {
 
 	GPIO_Config(&pwprueba);
 
+	//COnfiugracion ADC se pones un samplig period de 56 ya que ~= 100 mhz / 56 ~= 1.78 Mhz que es más que suficiente
 	uint8_t channels[2] = { ADC_CHANNEL_1, ADC_CHANNEL_4 };
 	channnel_0.channels = channels;
 	channnel_0.dataAlignment = ADC_ALIGNMENT_RIGHT;
 	channnel_0.numberOfChannels = 2;
 	uint8_t samplingPeriod[2] = { 0 };
-	samplingPeriod[0] = ADC_SAMPLING_PERIOD_480_CYCLES
+	samplingPeriod[0] = ADC_SAMPLING_PERIOD_56_CYCLES
 	;
-	samplingPeriod[1] = ADC_SAMPLING_PERIOD_480_CYCLES
+	samplingPeriod[1] = ADC_SAMPLING_PERIOD_56_CYCLES
 	;
 	channnel_0.samplingPeriod = samplingPeriod;
 	channnel_0.resolution = ADC_RESOLUTION_12_BIT;
@@ -467,7 +541,7 @@ void BasicTimer3_Callback(void) {
 	flagDatos = 1;
 }
 
-void USART2Rx_Callback(void) {
+void USART6Rx_Callback(void) {
 	rxData = getRxData();
 }
 
@@ -477,12 +551,14 @@ void adcComplete_Callback(void) {
 		adcIsCompleteCount++;
 		if (banderaImprimirAct) {
 			adcData[0][count256] = adcLastData1;
+			adcData2[0][count256] = adcLastData1/4095.0f;
 		}
 	} else {
 		adcLastData2 = getADC();
 		adcIsCompleteCount = 0;
 		if (banderaImprimirAct) {
 			adcData[1][count256] = adcLastData2;
+			adcData2[1][count256] = adcLastData2/4095.0f;
 			count256++;
 		}
 	}
@@ -515,9 +591,9 @@ void parseCommands(char *ptrBufferReception) {
 				"4) Changetrim #(0-> disminuir, 1-> aumentar) \n");
 
 		writeString(&handlerTerminal,
-				"5)  Analogos_frecuencia #(Valor de la frecuencia entre 10 y 20)khz\n");
+				"5)  adc_frecuencia #(Valor de la frecuencia entre 10 y 20)khz\n");
 		writeString(&handlerTerminal,
-				"6)  Analogos_arreglos (imprime los siguientes 256 datos)\n");
+				"6)  adc_datos (imprime los siguientes 256 datos)\n");
 		writeString(&handlerTerminal,
 				"Comandos relacionados al RTC\n"
 						"Tener en cuenta que no acepta datos incorrectos, si un dato es valido y el otro no solo se conservara el dato valido\n");
@@ -528,8 +604,12 @@ void parseCommands(char *ptrBufferReception) {
 		writeString(&handlerTerminal,
 				"10) RTC_set_fecha4 #(diaSemana), Lunes 1 ... Domingo 7 \n");
 		writeString(&handlerTerminal, "11) RTC_get_fecha \n");
-		writeString(&handlerTerminal, "12) Acelerometro_datos: Captura de datos e impresión se dejan listos para Fourier\n");
-		writeString(&handlerTerminal, "13) Acelerometro_FFT: FFT result \n");
+		writeString(&handlerTerminal,
+				"12) Ac_datos: Captura de datos e impresión se dejan listos para Fourier\n");
+		writeString(&handlerTerminal,
+				"13) Ac_FFT: FFT result (x = 0; y = 1,  z = 2) \n");
+		writeString(&handlerTerminal,
+				"14) FFT 1: FFT result (ADCdatos1 = 1 ADCdatos2 = 2) default ADCdatos1 \n");
 
 	}
 
@@ -599,12 +679,13 @@ void parseCommands(char *ptrBufferReception) {
 		} else {
 			writeString(&handlerTerminal, "Wrong number \n");
 		}
-	} else if (strcmp(cmd, "Analogos_frecuencia") == 0) {
+	} else if (strcmp(cmd, "adc_frecuencia") == 0) {
 
 		if (firstParameter <= 20 && firstParameter >= 10) {
 			sprintf(bufferData, "Se cambio la frecuencia de mustreo a %d \n",
 					firstParameter);
 			writeString(&handlerTerminal, bufferData);
+			freqAdc = firstParameter;
 			uint16_t auxFreq = (10000 / firstParameter);
 			updateFrequency(&pwmadc, auxFreq);
 
@@ -614,7 +695,7 @@ void parseCommands(char *ptrBufferReception) {
 		}
 	}
 
-	else if (strcmp(cmd, "Analogos_arreglos") == 0) {
+	else if (strcmp(cmd, "adc_datos") == 0) {
 		writeString(&handlerTerminal,
 				"Se enviaran los siguientes datos de amboas canales \n");
 		banderaImprimirAct = 1;
@@ -707,9 +788,105 @@ void parseCommands(char *ptrBufferReception) {
 		sprintf(bufferData, " Dia %d, Mes %d, Año %d \nSon las %d:%d:%d \n",
 				dias, meses, years, horas, minutos, segundos);
 		writeString(&handlerTerminal, bufferData);
+	} else if (strcmp(cmd, "Ac_datos") == 0) {
+		flag256Listos = 1;
+
 	}
-	else if (strcmp(cmd, "Acelerometro_datos") == 0) {
-		flag200Listos = 1;
+
+	/*  Presentar los datos de la frecuencia leída por el acelerómetro (CMSIS-FFT) */
+	else if (strcmp(cmd, "Ac_FFT") == 0) {
+		writeString(&handlerTerminal, bufferData);
+		stopTime = 0.0;
+
+		int j = 0;
+
+		sprintf(bufferData, "FFT \n");
+		writeString(&handlerTerminal, bufferData);
+		//Calcular la transformada de Fourier
+		if (firstParameter == 0) {
+			datos = DatoX256;
+		} else if (firstParameter == 1) {
+			datos = DatoY256;
+		} else {
+			writeString(&handlerTerminal,
+					"Parametro invalido, se manda el default Z \n");
+			datos = DatoZ256;
+		}
+		if (statusInitFFT == ARM_MATH_SUCCESS) {
+			arm_rfft_fast_f32(&config_Rfft_fast_f32, datos, transformedSignal,
+					ifftFlag);
+			arm_abs_f32(transformedSignal, transformedAbs, fttSize);
+
+			uint32_t indexMax = 0;
+			float FTT_Max = transformedAbs[1];
+			for (int i = 1; i < fttSize; i++) {
+				if (i % 2) {
+					if (transformedAbs[i] > FTT_Max) {
+						FTT_Max = transformedAbs[i];
+						indexMax = j;
+					}
+					sprintf(bufferData, "%u ; %#.6f\n", j,
+							2 * transformedAbs[i]);
+					writeString(&handlerTerminal, bufferData);
+					j++;
+				}
+			}
+
+			float frec = (indexMax * 200) / 256.0f;
+			sprintf(bufferData, "frecuency %.2f Hz \n", frec);
+			writeString(&handlerTerminal, bufferData);
+
+		} else {
+			writeString(&handlerTerminal, "FFT not initialized...");
+		}
+	}
+
+	/*  Presentar los datos de la frecuencia leída por el acelerómetro (CMSIS-FFT) */
+	else if (strcmp(cmd, "FFT") == 0) {
+		writeString(&handlerTerminal, bufferData);
+		stopTime = 0.0;
+
+		int j = 0;
+
+		sprintf(bufferData, "FFT \n");
+		writeString(&handlerTerminal, bufferData);
+		//Calcular la transformada de Fourier
+		if (firstParameter == 1) {
+			datos = adcData2[0];
+		} else if (firstParameter == 2) {
+			datos = adcData2[1];
+		} else {
+			writeString(&handlerTerminal,
+					"Parametro invalido, se manda el default ADCdata1 \n");
+			datos = adcData2[0];
+		}
+		if (statusInitFFT == ARM_MATH_SUCCESS) {
+			arm_rfft_fast_f32(&config_Rfft_fast_f32, datos, transformedSignal,
+					ifftFlag);
+			arm_abs_f32(transformedSignal, transformedAbs, fttSize);
+
+			uint32_t indexMax = 0;
+			float FTT_Max = transformedAbs[1];
+			for (int i = 1; i < fttSize; i++) {
+				if (i % 2) {
+					if (transformedAbs[i] > FTT_Max) {
+						FTT_Max = transformedAbs[i];
+						indexMax = j;
+					}
+					sprintf(bufferData, "%u ; %#.6f\n", j,
+							2 * transformedAbs[i]);
+					writeString(&handlerTerminal, bufferData);
+					j++;
+				}
+			}
+
+			float frec = (indexMax * freqAdc * 1000) / 256.0f;
+			sprintf(bufferData, "frecuency %.2f Hz \n", frec);
+			writeString(&handlerTerminal, bufferData);
+
+		} else {
+			writeString(&handlerTerminal, "FFT not initialized...");
+		}
 	}
 
 	else {
